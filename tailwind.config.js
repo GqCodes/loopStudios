22f9b36e04c5bcf8ae0a1d95043991ef4776e817
Chris Vargas <gqcodes@gmail.com>
2022-11-01 14:52:13 -0400

    inital commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      xxl: "1536px",
+    },
+    extend: {
+      fontFamily: {
+        sans: ["Josefin Sans", "sans-serif"],
+        alata: ["Alata"],
+      },
+      letterSpacing: {
+        widest: ".3em",
+      },
+    },
+  },
+  plugins: [],
+};
